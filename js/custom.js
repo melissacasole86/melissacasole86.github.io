08d19e7295a8872067c77650d5877c077bcae8e5
Melissa Casole <melissa.casole@fadv.com>
2022-06-27 16:55:36 -0400

    built header

--- js/custom.js
+++ js/custom.js
@@ -1,19 +1,19 @@
 'use strict'
 
-$(document).ready(function(){
-    // scripts for patterns
+// $(document).ready(function(){
+//     // scripts for patterns
 
-    $('.btn-primary').click(function(){
-        var email = $(this).closest('.email');
+//     $('.btn-primary').click(function(){
+//         var email = $(this).closest('.email');
 
-        if($(this).text()=='Select'){
-            $(this).text('Selected').prepend('<i class="fa fa-check-circle "></i>').addClass('selected')
-            email.addClass('selected');
-        } else {
-            $(this).text('Select').removeClass('selected')
-            email.removeClass('selected');
-        }
-    });
+//         if($(this).text()=='Select'){
+//             $(this).text('Selected').prepend('<i class="fa fa-check-circle "></i>').addClass('selected')
+//             email.addClass('selected');
+//         } else {
+//             $(this).text('Select').removeClass('selected')
+//             email.removeClass('selected');
+//         }
+//     });
 
 
 
