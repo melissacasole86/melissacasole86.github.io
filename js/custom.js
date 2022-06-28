1b4cee608b5953ecf78229b5f11914f1c83b8f6a
Melissa Casole <melissa.casole@fadv.com>
2022-06-28 16:00:55 -0400

    wip

--- js/custom.js
+++ js/custom.js
@@ -1,41 +1,42 @@
 'use strict'
 
- $(document).ready(function(){
-//     // scripts for patterns
+$(document).ready(function () {
+    //     // scripts for patterns
 
-//     $('.btn-primary').click(function(){
-//         var email = $(this).closest('.email');
+    //     $('.btn-primary').click(function(){
+    //         var email = $(this).closest('.email');
 
-//         if($(this).text()=='Select'){
-//             $(this).text('Selected').prepend('<i class="fa fa-check-circle "></i>').addClass('selected')
-//             email.addClass('selected');
-//         } else {
-//             $(this).text('Select').removeClass('selected')
-//             email.removeClass('selected');
-//         }
-//     });
+    //         if($(this).text()=='Select'){
+    //             $(this).text('Selected').prepend('<i class="fa fa-check-circle "></i>').addClass('selected')
+    //             email.addClass('selected');
+    //         } else {
+    //             $(this).text('Select').removeClass('selected')
+    //             email.removeClass('selected');
+    //         }
+    //     });
 
 
 
     //scripts for index
-
     // move bar in main nav to active link
-    
-    $('li a').click(function(event){
+
+    $('li').click(function (event) {
+       
+        // var url = $(this).find('a').attr('href');
         var bar = $('.bar');
         var liPos = $(this).offset().top;
-        event.preventDefault();
-        $(bar).offset({top:liPos});
+        $(bar).offset({
+            top: liPos
+        });
 
         $(this).addClass('active').closest('nav').find('.active').not(this).removeClass('active');
+        event.preventDefault();
     });
 
-    //animate main content full/small
-    
     // animate nav to close/open
 
-    $('.anchor').click(function(){
-        if($('main').hasClass('small')){
+    $('.anchor').click(function () {
+        if ($('main').hasClass('small')) {
             $('main').addClass('full').removeClass('small')
         } else {
             $('main').removeClass('full').addClass('small');
@@ -44,12 +45,12 @@
         $(this).toggleClass('rotate180');
         $('nav').toggleClass('closed');
     });
-    
 
+ //animate main content full/small
     // minimize left nav at mobile breakpoint
 
-    $(window).resize(function(){
-        if($(this).width() < 1024){
+    $(window).resize(function () {
+        if ($(this).width() < 1024) {
             $('.mainNav').addClass('closed')
             $('main').addClass('full').removeClass('small')
             $('.anchor').addClass('rotate180');
@@ -60,11 +61,3 @@
         }
     });
 });
-
-
-
-
-
-
-
-
